9b62038d463fd860015bee05ba11bfa8abd16ee0
Hermes (agora) <hermes.frangoudis@agora.io>
2024-04-02 15:52:25 -0400

    add tracks to media store when ready

--- src/TracksConfigure.tsx
+++ src/TracksConfigure.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, PropsWithChildren } from 'react'
-import { RtcPropsInterface } from './PropsContext'
-import { useLocalMicrophoneTrack, useLocalCameraTrack } from 'agora-rtc-react'
+import React, { useEffect, useRef, PropsWithChildren } from 'react'
+import { RtcPropsInterface, mediaStore } from './PropsContext'
+import { useLocalMicrophoneTrack, useLocalCameraTrack, ILocalAudioTrack, ILocalVideoTrack } from 'agora-rtc-react'
 import { TracksProvider } from './TracksContext'
 
 /**
@@ -9,6 +9,7 @@ import { TracksProvider } from './TracksContext'
 const TracksConfigure: React.FC<PropsWithChildren<Partial<RtcPropsInterface>>> = (props) => {
   const { localMicrophoneTrack, isLoading: isMicLoading, error: micError } = useLocalMicrophoneTrack()
   const { localCameraTrack, isLoading: isCameraLoading, error: cameraError } = useLocalCameraTrack()
+  const mediaStore = useRef<mediaStore>({})
   
   // Ready when the tracks are not in a loading state and did not encounter any errors while loading
   const ready = !isMicLoading && !isCameraLoading && !micError && !cameraError
@@ -19,6 +20,16 @@ const TracksConfigure: React.FC<PropsWithChildren<Partial<RtcPropsInterface>>> =
       console.error('Local tracks error:', { micError, cameraError })
     }
   },[micError, cameraError])
+
+  useEffect(() => {
+    if(ready) {
+      console.log(`Mic and Camera Tracks are ready: ${ready}`)
+      mediaStore.current[0] = {
+        audioTrack: localMicrophoneTrack as ILocalAudioTrack,
+        videoTrack: localCameraTrack as ILocalVideoTrack
+      }
+    } 
+  },[ready])
 
   return (
     <TracksProvider
