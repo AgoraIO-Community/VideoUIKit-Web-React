7997fcc740d1d7d0dd56a8a84383c23ff70ce70c
Hermes (agora) <hermes.frangoudis@agora.io>
2024-04-02 16:40:55 -0400

    update to react 18 createRoot

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,7 +1,10 @@
 import './index.css'
 
 import React from 'react'
-import ReactDOM from 'react-dom'
 import App from './App'
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const container = document.getElementById('root')
+const root = createRoot(container!)
+root.render(<App />)
+
