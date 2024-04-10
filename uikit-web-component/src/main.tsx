5c680704f693abdc45ab9327544d143938d25b79
Hermes (agora) <hermes.frangoudis@agora.io>
2024-04-10 10:31:00 -0400

    updated template

--- uikit-web-component/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './AgoraWebUIKitWrapper'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    {/*  */}
-  </React.StrictMode>,
-)
