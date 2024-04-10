8a5d7f012e3cd19d4b825c7e0cadfbc8e2c934d9
Hermes (agora) <hermes.frangoudis@agora.io>
2024-04-09 22:56:06 -0400

    removed vite defaults

--- uikit-web-component/src/main.tsx
+++ uikit-web-component/src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './AgoraWebUIKitWrapper'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {/*  */}
   </React.StrictMode>,
 )
